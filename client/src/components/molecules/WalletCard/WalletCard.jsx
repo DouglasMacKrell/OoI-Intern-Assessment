9d21ff9ecadb1b97572ca8ef124b9dd78dd18e9f
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-27 21:29:28 -0400

    removed Typography, Chip, updated import of usdFormatter and cryptoFormatter, exchange rate init

--- client/src/components/molecules/WalletCard/WalletCard.jsx
+++ client/src/components/molecules/WalletCard/WalletCard.jsx
@@ -1,42 +1,30 @@
 import React from "react";
 import Card from "../../atoms/Card/index";
-import Chip from "../../atoms/Chip/index";
-import Typography from "@material-ui/core/Typography";
-// import usdFormatter from "../../../util/index";
-// import cryptoFormatter from "../../../util/index";
+import CardAddress from "../../atoms/Text/CardAddress";
+import CardBalance from "../../atoms/Text/CardBalance";
+import CardTitle from "../../atoms/Text/CardTitle";
+import { usdFormatter, cryptoFormatter } from "../../../util/index";
 
 const WalletCard = ({ wallets, rate }) => {
-  const usdFormatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  });
-
-  const cryptoFormatter = (amount) => {
-    return Math.round(amount * 1e5) / 1e5;
-  };
 
   let storedWallets = [];
   let counter = 0;
+
   if (wallets.length > 0) {
     for (let wallet of wallets) {
       console.log(wallet);
       console.log(rate);
       let convertedBalance;
-      if (wallet.balance[2] === "e") {
-        convertedBalance = Math.round(wallet.balance * 1e5) / 1e5;
-      } else console.log(convertedBalance);
+      let exchangeRate = cryptoFormatter(wallet.balance / rate);
+      convertedBalance = usdFormatter.format(exchangeRate);
+      console.log(convertedBalance);
       storedWallets.push(
         <Card key={counter}>
-          <Typography variant="h3">Name: {wallet.name}</Typography>
-          <Typography gutterBottom color="textSecondary">
-            Address: {wallet.address}
-            <br />
-            Currency: {wallet.currency}
-            <br />
-            Balance: {wallet.balance}
-            <br />
-          </Typography>
+          <CardTitle>
+            NAME: {wallet.name} CURRENCY: {wallet.currency}
+          </CardTitle>
+          <CardBalance>BALANCE: {convertedBalance}</CardBalance>
+          <CardAddress>ADDRESS: {wallet.address}</CardAddress>
         </Card>
       );
       counter += 1;
